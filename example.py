9ca3e6897c00ed0d669e3e64c3bb974788ce7d5d
Allan Vestergaard <info@avz.dk>
2023-08-25 11:10:20 +0200

    Read og AddNew

---
+++ example.py
@@ -0,0 +1,27 @@
+from sparxdb import SparxDb, Object, Attribute
+import tomli
+
+
+def exRead():
+    sdb=SparxDb(conf['connstr'])
+    classes=sdb.getObjects(type="Class")
+    myclass=classes[1]
+    myattribute=myclass.attributes[0]
+    print(myclass)
+    print(myattribute)
+    print(myattribute.tags[0])
+
+def exAddNew():
+    sdb=SparxDb(conf['connstr'])
+    newobj=Object(Name="My New Class", Object_Type="Class", Package_ID=2)
+    sdb.add(newobj)
+
+
+if __name__ == '__main__':
+    try:
+        with open("conf_dev.toml", "rb") as f: conf = tomli.load(f)
+    except FileNotFoundError:
+        with open("conf.toml", "rb") as f: conf = tomli.load(f)
+
+    exRead()
+    exAddNew()
